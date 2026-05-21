15740b8cc45801867e205454de2d680b5990aea0
Workflow Engine Dev <dev@workflow-engine.local>
2026-05-21 23:50:53 +0800

    fix: 插件入口移到侧栏顶部(设置上方) + shrink-0防止被截断

--- src/pages/Dashboard.vue
+++ src/pages/Dashboard.vue
@@ -227,7 +227,20 @@ defineExpose({ loadList })
     </SidebarGroup>
   </SidebarContent>
 
-  <SidebarFooter>
+  <SidebarFooter class="shrink-0">
+    <SidebarMenuItem>
+      <SidebarMenuButton :tooltip="'🔌 插件'" @click="onPlugins">
+        <template #icon>
+          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+            <path d="M14.5 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V7.5L14.5 2z" />
+            <polyline points="14 2 14 8 20 8" />
+            <path d="M12 18v-6" />
+            <path d="M9 15l3-3 3 3" />
+          </svg>
+        </template>
+        🔌 插件
+      </SidebarMenuButton>
+    </SidebarMenuItem>
     <SidebarMenuItem>
       <SidebarMenuButton :tooltip="t('nav.settings')" @click="onSettings">
         <template #icon>
@@ -261,19 +274,6 @@ defineExpose({ loadList })
           </svg>
         </template>
         {{ t('common.import') }}
-      </SidebarMenuButton>
-    </SidebarMenuItem>
-    <SidebarMenuItem>
-      <SidebarMenuButton :tooltip="'插件'" @click="onPlugins">
-        <template #icon>
-          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-            <path d="M14.5 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V7.5L14.5 2z" />
-            <polyline points="14 2 14 8 20 8" />
-            <path d="M12 18v-6" />
-            <path d="M9 15l3-3 3 3" />
-          </svg>
-        </template>
-        🔌 插件
       </SidebarMenuButton>
     </SidebarMenuItem>
   </SidebarFooter>
